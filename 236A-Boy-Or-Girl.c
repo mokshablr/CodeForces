60488ecfa2916bcc92b75651dc308726029d5b34
Arjun Maniyani <arjunrmaniyani@gmail.com>
2023-08-18 23:37:25 +0530

    uploaded

--- 236A-Boy-Or-Girl.c
+++ 236A-Boy-Or-Girl.c
@@ -2,14 +2,16 @@
 #include<string.h>
 
 int main(){
-    char str[101], alph[27];
-    int alp=0, unique=0;
+    char str[101];
+    int unique=0; //to count number of unique chars
     scanf("%s", str);
 
     int len = strlen(str);
 
     for(int i=0; i<len; i++){
         int found = 0;
+
+        //as the loop iterates, the LAST OCCURENCES of the character will be unique.
         for(int j=i+1; j<len; j++){
             if(str[i] == str[j]){
                 found++;
